4dbc97abe063138d88654a63a41a3f0d8b5742b8
Diovane Maia <diovane.dm@gmail.com>
2021-05-09 21:31:37 -0300

    Create TransactionsTable component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { TransactionsTable } from "./components/TransactionsTable";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
     return (
         <>
+            <GlobalStyle />
+
             <Header />
             <Dashboard />
-            
-            <GlobalStyle />
         </>
     )
 }
